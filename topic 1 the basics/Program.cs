c400fdebe3ea661bc8ee602c31cee18700a9e156
339719510 <339719510@2260I-L29-004>
2024-09-05 09:29:17 -0400

    Add project files.

---
+++ topic 1 the basics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Security;
+
+namespace topic_1_the_basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   /*whatsup*/
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I am writing on the next line");
+            Console.Write("Hello, World!");
+            Console.WriteLine("I will print this on the same line");
+            Console.Write("Rip Johnny Hockey");
+            //Console.WriteLine creates a line under their statement and Console.Write will write on the line after the previous Console.writeLine
+            Console.Write(" he was a Legend");
+
+            Console.WriteLine("");
+            Console.WriteLine("Hello! My name is Bailey");
+            Console.WriteLine("");
+            Console.WriteLine("comments are used to communicate when coding without disturbing the project itself when running.");
+            Console.WriteLine("");
+            Console.WriteLine("BBBB  H   H");
+            Console.WriteLine("B   B H   H");
+            Console.WriteLine(
+        }
+    }
+}
